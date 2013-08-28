9363a7e92b46fdf8bdd5a728f48b169d886bff05
Eugene Khrustalev <eugene.khrustalev@gmail.com>
2013-08-29 00:51:12 +0400

    function next_token works

--- 3.8/parser.erl
+++ 3.8/parser.erl
@@ -2,36 +2,45 @@
 -export([parse/1]).
 
 %% Parse a string expression and return instruction-set for VM
-parse([]) ->
-    {};
 parse(Expr) ->
+    parse(Expr, {num, 0}, op_none).
+
+parse([], _, _) ->
+    {};
+parse(Expr, LastOp, LastOpcode) ->
     case next_token(Expr) of
-        {Opcode, TailExpr} when is_atom(Opcode) ->
-            io:format("opcode=~p, tail=~p~n", [Opcode, TailExpr]),
-            parse(TailExpr);
-        {Number, TailExpr} ->
-            io:format("number=~p, tail=~p~n", [Number, TailExpr]),
-            parse(TailExpr)
+        {op_neg, TailExpr} ->
+            {op_neg, parse(TailExpr, LastOp, LastOpcode)};
+        {Opcode, TailExpr} when LastOpcode == op_none ->
+            parse(TailExpr, LastOp, Opcode);
+        {num, Number, TailExpr} ->
+            case LastOpcode of
+                op_none when LastOp == 0 -> parse(TailExpr, {num, Number}, LastOpcode);
+                Opcode -> {Opcode, LastOp, {num, Number}}
+            end
     end.
 
 
 %% Extract next token from string
 next_token([]) ->
     {};
-next_token(List) ->
-    next_token(List, []).
-
-next_token([C | Expr], Acc) ->
-    io:format("C=~p, Expr=~p, Acc=~p~n", [C, Expr, Acc]),
+next_token([C | Expr]) ->
+    % io:format("C=~p, Expr=~p~n", [C, Expr]),
     if
-        C == $( -> {expr_start, Expr};
-        C == $) -> {expr_end, Expr};
-        C == $+ -> {op_plus, Expr};
-        C == $- -> {op_minus, Expr};
-        C == $/ -> {op_divide, Expr};
-        C == $* -> {op_multiply, Expr};
-        C == $~ -> {unary_minus, Expr};
-        (C >= $0) and (C =< $9) -> next_token(Expr, [C | Acc]);
-        (C >= $\r) or (C =< $\n) or (C =< $\t) or (C =< 32) -> next_token(Expr, []);
-        true -> {Acc, Expr}
+        C == $( -> {op_beg, Expr};
+        C == $) -> {op_end, Expr};
+        C == $+ -> {op_add, Expr};
+        C == $- -> {op_sub, Expr};
+        C == $/ -> {op_div, Expr};
+        C == $* -> {op_mul, Expr};
+        C == $~ -> {op_neg, Expr};
+        (C >= $0) and (C =< $9) -> take_number(Expr, list_to_integer([C]) );
+        (C == $\r) or (C == $\n) or (C == $\t) or (C == $ ) -> next_token(Expr);
+        true -> throw({invalid_character, C})
     end.
+
+%% Extract number from string
+take_number([H | Expr], N) when (H >= $0) and (H =< $9) ->
+    take_number(Expr, N*10 + list_to_integer([H]) );
+take_number(Expr, N) -> 
+    {num, N, Expr}.
